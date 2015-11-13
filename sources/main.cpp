345974097fa9d888035f19ae1b8a2eadeead2337
tomcombriat <tomcombriat@live.fr>
2015-11-13 16:25:48 +0100

    Better memory allocation

--- sources/main.cpp
+++ sources/main.cpp
@@ -40,7 +40,7 @@ int main( int argc, char** argv )
 {
 
   system("clear");
-  
+
   if( argc < 3)
     {
       cout <<"\n\nUsage: ./PTV Video to load output_File -arg" << endl;
@@ -50,7 +50,7 @@ int main( int argc, char** argv )
 
 
   cout<<"\n\n     Copyright 2014,2015 Thomas Combriat\nThis program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.\n\n This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.\n\n You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.\n\n";
-  
+
 
   bool mode_test=false;
   if (argc==4)
@@ -59,17 +59,17 @@ int main( int argc, char** argv )
       else cout<<"\n Last argument unknown... Assuming non-test mode\n\n";
     }
 
-  
+
   cout<<"\n\n    Welcome to FAST (a Fast And Simple Tracker), developped at the LIPhy (Grenoble - France).\n    This program is distributed under the licence GNU GPLv3.\n\n\n";
 
   fstream sortie(argv[2],ios::out);
   fstream log("log.txt",ios::out);  //for further developments
   double search_radius=0;
   unsigned int NB_remanence=50,area_max=1000,area_min=0;
-  vector<Track> tracks; 
+  vector<Track> tracks;
   char key;
   unsigned int i=0,kernel_size=3,derivative_size,gap;
- 
+
   cout<<"  Press enter to continue...\n\n";
   cin.ignore();
 
@@ -86,18 +86,21 @@ int main( int argc, char** argv )
       cout<<"An error occured while opeing the source file... \nNow exiting"<<endl;
       return 0;
     }
-  
 
- 
+
+
   int NB_Frame=video.get(CV_CAP_PROP_FRAME_COUNT);
-  cout<<NB_Frame<<" images to analyse"<<endl;
-  Mat output[NB_Frame+1];
-  
+
+cout<<NB_Frame<<" images to analyse"<<endl;
+  Mat * output;
+  output=new Mat[NB_Frame+1];
+
+
   Mat blurred_img,LOG_img[1],mean_img,frame32f,tamp,thresholded,img,input_bg;
- 
+
 
   namedWindow( "Display", WINDOW_NORMAL);// Create a window for display.
-  
+
 
   bool new_background=true;
 
@@ -106,11 +109,11 @@ int main( int argc, char** argv )
   cin>>new_background;
   */
 
-  
+
   mean_img.convertTo(mean_img,6);
   if( new_background)
     {
-      cout<<"Calculating the background..."<<endl; 
+      cout<<"Calculating the background..."<<endl;
       while(video.read(img))
 	{
 	  if (i%100==0)
@@ -120,7 +123,7 @@ int main( int argc, char** argv )
 	    }
 	  tamp=img.clone();
 	  tamp.convertTo(tamp,6);
-	
+
 	  tamp=tamp*1./NB_Frame;
 	  if (i==0) mean_img=tamp.clone();
 	  else mean_img+=tamp.clone();
@@ -133,8 +136,8 @@ int main( int argc, char** argv )
       input_bg=imread("background.jpg",-1);
       mean_img=input_bg.clone();
     }
-  
-  
+
+
   mean_img.convertTo(mean_img,0);
 
   cout<<"\n\n  LOG detector: informations required:\n";
@@ -148,7 +151,7 @@ int main( int argc, char** argv )
 
   log<<argv[0]<<" "<<argv[1]<<" "<<argv[2]<<" "<<mode_test<<endl;
   log<<"Blur size = "<<kernel_size<<"\nDerivative size = "<<derivative_size<<"\nSearch radius = "<<search_radius<<endl;
-	
+
   VideoCapture video2(argv[1]);
   video2.read(img);
 
@@ -156,7 +159,7 @@ int main( int argc, char** argv )
   GaussianBlur(mean_img-img,blurred_img,Size(kernel_size,kernel_size),0,0,BORDER_DEFAULT);
   cvtColor(blurred_img, blurred_img, CV_RGB2GRAY);
   Laplacian(blurred_img,LOG_img[0],CV_32F,derivative_size,-1,120,BORDER_DEFAULT);
-  
+
   bool satisfied=false;
   double _threshold;
   while (!satisfied)
@@ -184,7 +187,7 @@ int main( int argc, char** argv )
   cout<<"    - number of detected particles: "<<points[0].size()<<endl;
   cout<<"    - mean size/std: "<<points_mean(points[0])<<"  "<<points_std(points[0])<<endl;
 
-      
+
   while (!satisfied)
     {
       imshow("Display",thresholded);
@@ -206,7 +209,7 @@ int main( int argc, char** argv )
     }
   cout<<"     Gap closing (null or positive integer) >> ";
   cin>>gap;
-      
+
   i=1;
   cout<<"  Detection..."<<endl;
   while(i<NB_Frame)
@@ -215,7 +218,7 @@ int main( int argc, char** argv )
       if (i%100==0)
 	{
 	  cout<<"\r"<<i<<"/"<<NB_Frame;
-	  fflush(stdout);	  
+	  fflush(stdout);
 	}
 
       GaussianBlur(mean_img-img,blurred_img,Size(kernel_size,kernel_size),0,0,BORDER_DEFAULT);
@@ -223,7 +226,7 @@ int main( int argc, char** argv )
       Laplacian(blurred_img,LOG_img[0],CV_32F,derivative_size,-1,120,BORDER_DEFAULT);
       threshold(LOG_img[0],thresholded,_threshold,255,THRESH_BINARY);
       find_particules(thresholded,0,points[i]);
-      
+
       if (mode_test)
 	{
 	  tamp=img.clone();
@@ -233,20 +236,20 @@ int main( int argc, char** argv )
 		{
 	      circle(tamp,Point(points[i][j].center_position()[1],points[i][j].center_position()[0]),sqrt(points[i][j].area()),Scalar(0xFFFF),1,8,0);
 		}}
-       
+
 	  imshow("Display",tamp);
 	  cv::waitKey(50);
 	}
-       
+
       i++;
     }
-      
+
   cout<<endl<<endl<<"  Linking particles..."<<endl;
   link_particules(points,tracks,search_radius, NB_Frame,0,100,gap);
 
   cout<<endl<<endl<<"Done!  "<<tracks.size()<<" tracks have benn created!"<<endl;
 
-      
+
   cout<<"Écriture des fichiers..."<<endl;
   sortie<<"#Tracks_ID,X,Y,T"<<endl;
   for (unsigned int i=0;i<tracks.size();i++)
